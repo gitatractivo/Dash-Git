f6a4d72596d24448f48334929b65ea8c6c47a6f9
himalaya-10 <98940477+himalaya-10@users.noreply.github.com>
2024-04-03 13:15:29 +0530

    announcements started

--- src/app/announcement/page.tsx
+++ src/app/announcement/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import * as React from "react"
+import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+
 
 export default function Page() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const router = useRouter();
+    return (
+        <div className="m-40">
+        <Card className=" m-10 text-orange-400 border-orange-400 hover:scale-105 transition-all 0.5s ease-in-out cursor-pointer" onClick={() => {
+            // router.push("/placement/createAnnouncement");
+          }}>
+            <CardHeader>
+                <CardTitle>Create Announcement</CardTitle>
+                <CardDescription>Create a new announcement</CardDescription>
+            </CardHeader>
+        </Card>
+        <Card className=" m-10 text-orange-400 border-orange-400 hover:scale-105 transition-all 0.5s ease-in-out cursor-pointer">
+            <CardHeader>
+                <CardTitle>Create Google Form</CardTitle>
+                <CardDescription>Create a new announcement</CardDescription>
+            </CardHeader>
+        </Card>
+        <Card className=" m-10 text-orange-400 border-orange-400 hover:scale-105 transition-all 0.5s ease-in-out cursor-pointer">
+            <CardHeader>
+                <CardTitle>Checkout Prev Announcements</CardTitle>
+                <CardDescription>Create a new announcement</CardDescription>
+            </CardHeader>
+        </Card>
+        </div>
+
+    )
 }
